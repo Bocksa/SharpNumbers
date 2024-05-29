4c3ad4ff061777c11a0912f0e71254649af61a1b
Cian <cian_mc_namara@hotmail.com>
2024-05-30 00:32:34 +0100

    big big number

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace SharpNumbers {
     internal class Program {
         static void Main(string[] args) {
-            usuperint foo = new usuperint(99);
-            usuperint bar = new usuperint(99);
+            usuperint foo = new usuperint(9999999999999999999);
+            usuperint bar = new usuperint(9999999999999999999);
 
             foo = foo.add(bar);
 
