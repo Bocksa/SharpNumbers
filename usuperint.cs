b0a3cf8897ef9ec25c2a7160bedc840dba5597c2
Cian <cian_mc_namara@hotmail.com>
2024-09-27 22:47:16 +0100

    Addition added

--- usuperint.cs
+++ usuperint.cs
@@ -6,16 +6,52 @@ using System.Threading.Tasks;
 
 namespace SharpNumbers {
     class usuperint {
-        List<int> split_number = new List<int>();
-
-        /*
-         * Inserts all input characters into the split_number list in reverse order.
-         */
-
+        private List<int> split_number = new List<int>();
         public usuperint(string number) {
-            for (int i = number.Length - 1;  i >= 0; i--) {
-                split_number.Add(int.Parse(number[i].ToString()));
+            split_number = ConvertFromStringToIndexOrderList(number);
+        }
+        public usuperint(long number) {
+            split_number = ConvertFromStringToIndexOrderList(number.ToString());
+        }
+        private usuperint() {
+            /*
+             * This is not public because I don't see a reason why anyone might need it.
+             * This is purely used for creating temporary usuperints for math operations.
+             */
+        }
+        public usuperint Add(usuperint n2) {
+            usuperint temp = new usuperint();
+            if (split_number.Count > n2.split_number.Count) {
+                for (int i = 0; i < n2.split_number.Count; i++) {
+                    temp.split_number.Add(n2.split_number[i] + split_number[i]);
+                }
+            } else {
+                for (int i = 0; i < split_number.Count; i++) {
+                    temp.split_number.Add(n2.split_number[i] + split_number[i]);
+                }
             }
+
+            temp.Clean();
+
+            return temp;
+        }
+
+        private List<int> ConvertFromStringToIndexOrderList(string number) {
+            /*
+             * Loops through a string from last to first character and inserts the characters as integers into a list.
+             */
+            List<int> temporaryList = new List<int>();
+            for (int i = number.Length - 1; i >= 0; i--) {
+                temporaryList.Add(int.Parse(number[i].ToString()));
+            }
+            return temporaryList;
+        }
+        /* 
+         * Before writing this only god knew the horrors that would come from this.
+         * Soon only god will know the journey I went on by writing this.
+         */
+        private void Clean() {
+
         }
     }
 }
